47412a9fd5429ef5dc7e9c64f6eeecde81b26cb9
Daniel Palmer <daniel@thingy.jp>
2026-06-07 21:13:02 +0900

    Bump versions of submodules, starting towards a bootable linux

--- buildlinux.sh
+++ buildlinux.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-make -C linux ARCH=m68k CROSS_COMPILE=`realpath ./buildroot/output/host/bin/m68k-linux-` virt_mc68000_defconfig
+make -C linux ARCH=m68k CROSS_COMPILE=`realpath ./buildroot/output/host/bin/m68k-linux-` megadrive_defconfig
 make -C linux ARCH=m68k CROSS_COMPILE=`realpath ./buildroot/output/host/bin/m68k-linux-` vmlinux -j8
+./buildroot/output/host/bin/m68k-linux-strip linux/vmlinux
+
+#make -C linux ARCH=m68k CROSS_COMPILE=`realpath ./buildroot/output/host/bin/m68k-linux-` menuconfig
+#make -C linux ARCH=m68k CROSS_COMPILE=`realpath ./buildroot/output/host/bin/m68k-linux-` savedefconfig
