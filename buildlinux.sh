3cd9fbd52a12698e0b22da66c9600c0f58c76e50
Daniel Palmer <daniel@thingy.jp>
2026-06-18 18:13:10 +0900

    First booting version.

--- buildlinux.sh
+++ buildlinux.sh
@@ -4,6 +4,7 @@ set -e
 set -u
 
 make -C linux ARCH=m68k CROSS_COMPILE=`realpath ./buildroot/output/host/bin/m68k-linux-` megadrive_defconfig
+#make -C linux ARCH=m68k CROSS_COMPILE=`realpath ./buildroot/output/host/bin/m68k-linux-` clean
 make -C linux ARCH=m68k CROSS_COMPILE=`realpath ./buildroot/output/host/bin/m68k-linux-` vmlinux -j8
 ./buildroot/output/host/bin/m68k-linux-strip linux/vmlinux
 lz4 -f -B65536 --best linux/vmlinux linux/vmlinux.lz4
