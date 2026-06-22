e2c47efbb90774b4321112fe462ea8494c84e7fe
Daniel Palmer <daniel@thingy.jp>
2026-06-22 22:41:05 +0900

    Some bumps and build qemu too

--- runqemu.sh
+++ runqemu.sh
@@ -5,4 +5,4 @@ cp u-boot/u-boot.bin linux/vmlinux.lz4 smolutils/m68k.erofs everdrive-sd/
 #cpulimit -l 10 --
 
 qemu-build/qemu-system-m68k -M megadrive -bios everdrive-sd/u-boot.bin \
-	-serial stdio -global md-everdrive.dir=everdrive-sd/ -icount shift=10,align=on -s
+	-serial stdio -global md-everdrive.dir=everdrive-sd/ -global md-everdrive.resp-delay-us=1 -icount shift=10,align=on -s
